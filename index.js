551e7397b37eaceab58e73c6325866ba44c377ee
Megan Marchetti <48037645+meganmarchetti@users.noreply.github.com>
2021-12-29 17:14:48 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = "3400";
+
+app.set("view engine", "ejs");
+
+app.get("/", (request, response) => {
+    response.render("home.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get("/:qn", (request, response) => {
+    const q = request.params["qn"];
+    response.render(""+q+".ejs");
+});
+
+app.get("/:cn", (request, response) => {
+    const c = request.params["cn"];
+    response.render(""+c+".ejs");
+});
