df65e927142db4ba28eee0577cbef9c618b9dd91
Megan Marchetti <48037645+meganmarchetti@users.noreply.github.com>
2021-12-30 12:11:05 -0500

    adding styling
    added css file for styles

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = "3400";
-
+app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
 app.get("/", (request, response) => {
